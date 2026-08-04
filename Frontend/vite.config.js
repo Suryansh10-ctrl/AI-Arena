4e9c4521b2e0c80ec31c77cf0af9776cfe32ae78
suryansh sharma <ss8737021@gmail.com>
2026-08-04 22:43:20 +0530

    commit changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://ai-arena-tv6a.onrender.com',
         changeOrigin: true,
       },
       '/invoke': {
-        target: 'http://localhost:3000',
+        target: 'https://ai-arena-tv6a.onrender.com',
         changeOrigin: true,
       },
     },

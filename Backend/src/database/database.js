9e1cde7ae3ffc88e241fc41ed603d8ead647c9ce
suryansh sharma <ss8737021@gmail.com>
2026-08-05 23:47:27 +0530

    commit chnages

--- Backend/src/database/database.js
+++ Backend/src/database/database.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose"
+import dns from "dns"
+
+// Use public DNS servers to resolve MongoDB SRV records if local router/ISP DNS fails
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
     try {

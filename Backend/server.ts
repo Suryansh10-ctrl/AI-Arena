2d43d3af89fb90de8d169aed19e5fb58ede5723b
suryansh sharma <ss8737021@gmail.com>
2026-08-05 23:50:02 +0530

    commit changes

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "1.1.1.1", "8.8.4.4"]);
+
 import dotenv from "dotenv"
 dotenv.config()
 console.log("JWT_SECRET:", process.env.JWT_SECRET ? "Loaded" : "Missing");
